ff8196ed703bdd80888b7abb4a5754a9afe6515b
jacek <jacek.a.domanski@gmail.com>
2019-04-09 23:56:31 +0200

    makeFirstFun added

--- src/hofA.kt
+++ src/hofA.kt
@@ -4,7 +4,7 @@ import java.lang.System.console
 // show how hof works
 
 
-fun StartA(){
+fun StartB(){
     var total = 0
     var count = 10
     while (count <= 10) {
@@ -15,6 +15,16 @@ fun StartA(){
 //    console.log(total) // this is java
 }
 
+// this function is called in Main.kt
 fun Test_hofA(){
-    println ("Test_hofA start")
+    println ("Test_hofA start, then function StartA")
+    StartA()
 }
+
+fun StartA() {
+    val funFF = makeFirstFun(3)
+    println("var funFF = makeFirstFun(3); funFF(2): ${funFF(2)}")
+}
+
+
+fun makeFirstFun (baseNum : Int): (Int)->Int = {num2 -> baseNum*100 + num2*10 }
