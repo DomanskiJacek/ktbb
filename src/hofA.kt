c202ba8d0c3217e11dc593a15881e0fbd8d75d88
jacek <jacek.a.domanski@gmail.com>
2019-04-10 00:31:43 +0200

    applyDis added

--- src/hofA.kt
+++ src/hofA.kt
@@ -18,13 +18,26 @@ fun StartB(){
 // this function is called in Main.kt
 fun Test_hofA(){
     println ("Test_hofA start, then function StartA")
+    println("todo: double round, string split and similiar (or rather more useful string functions)")
     StartA()
 }
 
 fun StartA() {
     val funFF = makeFirstFun(3)
     println("var funFF = makeFirstFun(3); funFF(2): ${funFF(2)}")
+    // another way of defining the function
+    val funMul2 = { par1: Int-> par1*2}
+    val funDiscount = { par1:Double ->  par1*0.95}
+    val currentPrices = arrayOf(101.5, 115.0, 198.9)
+
+    applyDis(currentPrices, funDiscount)
+
+
 }
 
 
 fun makeFirstFun (baseNum : Int): (Int)->Int = {num2 -> baseNum*100 + num2*10 }
+
+fun applyDis (curPr : Array<Double>, funNewPrice: (Double) -> Double){
+    curPr.forEach { println("Current $it, new price ${funNewPrice(it)}") }
+}
